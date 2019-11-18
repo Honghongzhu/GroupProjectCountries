1329fe7ae842e808396515277b93d631e4cba72a
Honghongzhu <honghong033@hotmail.com>
2019-11-18 14:31:36 +1100

    rename folder

--- app/src/main/java/com/example/groupprojectcountries/database/Country.java
+++ app/src/main/java/com/example/groupprojectcountries/database/Country.java
@@ -4,5 +4,5 @@ import androidx.room.Entity;
 
 @Entity
 public class Country {
-    
+
 }
--- app/src/main/java/com/example/groupprojectcountries/play/RegionRecyclerFragment.java
+++ app/src/main/java/com/example/groupprojectcountries/play/RegionRecyclerFragment.java
@@ -26,7 +26,7 @@ public class RegionRecyclerFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_city_recycler, container, false);
+        return inflater.inflate(R.layout.fragment_region_recycler, container, false);
     }
 
 }
