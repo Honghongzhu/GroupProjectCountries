cf0b8707f2c4e1b31ba4a6a4d4c4778c772d9365
Honghongzhu <honghong033@hotmail.com>
2019-11-18 14:39:49 +1100

    test git

--- app/src/main/java/com/example/groupprojectcountries/database/Country.java
+++ app/src/main/java/com/example/groupprojectcountries/database/Country.java
@@ -1,8 +1,10 @@
 package com.example.groupprojectcountries.database;
 
 import androidx.room.Entity;
+import androidx.room.PrimaryKey;
 
 @Entity
 public class Country {
-
+    @PrimaryKey
+    private int number;
 }
