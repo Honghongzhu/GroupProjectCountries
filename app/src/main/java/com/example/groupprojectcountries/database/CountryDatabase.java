939cc3fa12e103d8c2799ebcbbf2256b0307b787
Honghongzhu <honghong033@hotmail.com>
2019-11-18 14:08:01 +1100

    small change

--- app/src/main/java/com/example/groupprojectcountries/database/CountryDatabase.java
+++ app/src/main/java/com/example/groupprojectcountries/database/CountryDatabase.java
@@ -3,5 +3,5 @@ package com.example.groupprojectcountries.database;
 import androidx.room.Database;
 
 @Database(entities = {Country.class}, version = 1)
-public class CountryDatabse {
+public class CountryDatabase {
 }
