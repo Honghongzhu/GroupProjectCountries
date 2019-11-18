d2f66ed12d8c94d15f711647610eda8433bbe728
Honghongzhu <honghong033@hotmail.com>
2019-11-18 15:11:18 +1100

    updated coutry entity

--- app/src/main/java/com/example/groupprojectcountries/database/Country.java
+++ app/src/main/java/com/example/groupprojectcountries/database/Country.java
@@ -1,10 +1,27 @@
 package com.example.groupprojectcountries.database;
 
+import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
 @Entity
 public class Country {
     @PrimaryKey
-    private int number;
+    private int id;
+
+    @ColumnInfo(name = "Name")
+    private String name;
+
+    @ColumnInfo(name = "Capital")
+    private String capital;
+
+    @ColumnInfo(name = "Region")
+    private String region;
+
+    @ColumnInfo(name = "Flag")
+    private String flag;
+
+    @ColumnInfo(name = "Score")
+    private int score;
+
 }
