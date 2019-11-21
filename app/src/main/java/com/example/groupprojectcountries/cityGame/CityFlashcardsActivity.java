2d0fa4bb2279cd1985afc36385534ec426556ade
Honghongzhu <honghong033@hotmail.com>
2019-11-21 15:54:32 +1100

    flashcards working for cities level 1

--- app/src/main/java/com/example/groupprojectcountries/cityGame/CityFlashcardsActivity.java
+++ app/src/main/java/com/example/groupprojectcountries/cityGame/CityFlashcardsActivity.java
@@ -1,9 +1,13 @@
 package com.example.groupprojectcountries.cityGame;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -13,8 +17,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import com.example.groupprojectcountries.R;
+import com.example.groupprojectcountries.cityGame.completed.CityReadyToPracticeActivity;
 import com.example.groupprojectcountries.database.AppDatabase;
 import com.example.groupprojectcountries.database.Country;
+import com.example.groupprojectcountries.play.LevelActivity;
 
 import java.util.List;
 
@@ -22,6 +28,12 @@ public class CityFlashcardsActivity extends AppCompatActivity {
 
     private TextView countryName;
     private TextView capitalCity;
+    private ImageButton next;
+    private int counter;
+    private int amount;
+    private String region;
+    private String category;
+    private String level;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -30,27 +42,47 @@ public class CityFlashcardsActivity extends AppCompatActivity {
 
         countryName = findViewById(R.id.country_name_flash);
         capitalCity = findViewById(R.id.country_city_flash);
+        next = findViewById(R.id.next_city);
 
         AppDatabase db = AppDatabase.getInstance(this);
-        String region = getIntent().getStringExtra("REGION");
-        String category = getIntent().getStringExtra("CATEGORY"); //not necessary
-        String level = getIntent().getStringExtra("LEVEL");
+        region = getIntent().getStringExtra("REGION");
+        category = getIntent().getStringExtra("CATEGORY"); //not necessary
+        level = getIntent().getStringExtra("LEVEL");
         System.out.println("Region:" + region + " Category:" + category + " Level:" + level);
 
         List<Country> countryList = db.countryDao().getCountriesByRegion(region);
 
-        int amount = countryList.size()/4;
+        amount = countryList.size()/4;
 
-        List<Country> subListOne = countryList.subList(0, amount);
-        List<Country> subListTwo = countryList.subList(amount, amount*2);
-        List<Country> subListThree = countryList.subList(amount*2, amount*3);
-        List<Country> subListFour = countryList.subList(amount*3, countryList.size());
+        final List<Country> subListOne = countryList.subList(0, amount);
+        final List<Country> subListTwo = countryList.subList(amount, amount*2);
+        final List<Country> subListThree = countryList.subList(amount*2, amount*3);
+        final List<Country> subListFour = countryList.subList(amount*3, countryList.size());
 
         switch (level){
             case "1":
                 System.out.println("1");
                 countryName.setText(subListOne.get(0).getName());
                 capitalCity.setText(subListOne.get(0).getCapital());
+                counter = 0;
+                next.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Context context =  v.getContext();
+                        if(counter!=subListOne.size()) {
+                            countryName.setText(subListOne.get(counter).getName());
+                            capitalCity.setText(subListOne.get(counter).getCapital());
+                            counter++;
+                        }
+                        else{
+                            Intent intent = new Intent(context, CityReadyToPracticeActivity.class);
+                            intent.putExtra("REGION", region);
+                            intent.putExtra("CATEGORY", "capitalCities");
+                            intent.putExtra("LEVEL", "1");
+                            context.startActivity(intent);
+                        }
+                    }
+                });
                 break;
             case "2":
                 System.out.println("2");
